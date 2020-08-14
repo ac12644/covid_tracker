b3814b8e0d6293af11002f96a27fa7bc6cff6838
Abhishek Chauhan <ac12644@Abhisheks-MacBook-Air.local>
2020-08-14 20:19:29 +0530

    Updates

---
+++ precache-manifest.011e5ed70a99820d1ddc069234ea2d50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a4d101a45134a2caf52ddbb665ebc1",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "96ec6e55d3a1f51a03da",
+    "url": "/covid_tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "92ded1fe66cf77f70ae1",
+    "url": "/covid_tracker/static/css/main.c0c9146e.chunk.css"
+  },
+  {
+    "revision": "96ec6e55d3a1f51a03da",
+    "url": "/covid_tracker/static/js/2.64e03c9a.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid_tracker/static/js/2.64e03c9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ded1fe66cf77f70ae1",
+    "url": "/covid_tracker/static/js/main.b9055c47.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  }
+]);
